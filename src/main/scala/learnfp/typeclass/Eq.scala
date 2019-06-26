5b189eda4065343051e32678704c45b0e3635d90
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2019-06-26 13:21:21 +0900

    solve typeclass exercises

--- src/main/scala/learnfp/typeclass/Eq.scala
+++ src/main/scala/learnfp/typeclass/Eq.scala
@@ -1,31 +1,37 @@
 package learnfp.typeclass
 
 trait Eq[A] {
-  def eq(lhs:A, rhs:A):Boolean
+  def eq(lhs: A, rhs: A): Boolean
 }
 
-object Eq {
-  def eq[A](lhs:A, rhs:A)(implicit eqt:Eq[A]) = eqt.eq(lhs, rhs)
-}
+// I think the following is redundant (probably?)
+// object Eq {
+//   def eq[A](lhs: A, rhs: A)(implicit eqt: Eq[A]) = eqt.eq(lhs, rhs)
+// }
 
-class EqOps[A](lhs:A)(implicit eqt:Eq[A]) {
-  def ====(rhs:A):Boolean = eqt.eq(lhs, rhs)
+class EqOps[A](lhs: A)(implicit eqt: Eq[A]) {
+  def ====(rhs: A): Boolean = eqt.eq(lhs, rhs)
 }
 
 object EqOps {
-  implicit def toEqOps[A](lhs:A)(implicit eqt:Eq[A]) = new EqOps(lhs)
+  implicit def toEqOps[A](lhs: A)(implicit eqt: Eq[A]) = new EqOps(lhs)
 }
 
 object EqInstances {
   implicit val intEqInstance = new Eq[Int] {
-    override def eq(lhs: Int, rhs: Int): Boolean = ???
+    override def eq(lhs: Int, rhs: Int): Boolean = lhs == rhs
   }
 
   implicit val stringEqInstance = new Eq[String] {
-    override def eq(lhs: String, rhs: String): Boolean = ???
+    override def eq(lhs: String, rhs: String): Boolean = lhs == rhs
   }
 
-  implicit def listEqInstance[A](implicit eqt:Eq[A]) = new Eq[List[A]] {
-    override def eq(lhs: List[A], rhs: List[A]): Boolean = ???
+  implicit def listEqInstance[A: Eq] = new Eq[List[A]] {
+    override def eq(lhs: List[A], rhs: List[A]): Boolean =
+    (lhs, rhs) match {
+      case (l::Nil, r::Nil) => implicitly[Eq[A]].eq(l, r)
+      case (l::ltail, r::rtail) => implicitly[Eq[A]].eq(l, r) && eq(ltail, rtail)
+      case (_, _) => false
+    }
   }
 }
--- src/main/scala/learnfp/typeclass/TotalOrder.scala
+++ src/main/scala/learnfp/typeclass/TotalOrder.scala
@@ -5,16 +5,19 @@ trait TotalOrder[A] {
 }
 
 object TotalOrderInstances {
-  implicit val intInstance:TotalOrder[Int] = new TotalOrder[Int] {
-    override def less(lhs: Int, rhs: Int): Boolean = ???
+  implicit val intInstance: TotalOrder[Int] = new TotalOrder[Int] {
+    override def less(lhs: Int, rhs: Int): Boolean = lhs < rhs
   }
 
-  implicit val stringInstance:TotalOrder[String] = new TotalOrder[String] {
-    override def less(lhs: String, rhs: String): Boolean = ???
+  implicit val stringInstance: TotalOrder[String] = new TotalOrder[String] {
+    override def less(lhs: String, rhs: String): Boolean = lhs < rhs
   }
 
-  implicit def listInstance[T](implicit suborder:TotalOrder[T]):TotalOrder[List[T]] = new TotalOrder[List[T]] {
-    override def less(lhs: List[T], rhs: List[T]): Boolean = ???
+  implicit def listInstance[T: TotalOrder]: TotalOrder[List[T]] = new TotalOrder[List[T]] {
+    override def less(lhs: List[T], rhs: List[T]): Boolean =
+    lhs.zip(rhs).forall {
+      (tuple) => implicitly[TotalOrder[T]].less(tuple._1, tuple._2)
+    }
   }
 }
 
